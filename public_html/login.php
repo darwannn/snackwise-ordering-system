5a07d3e4f38f180f445326a93cb7be45e1913b24
clarencetinator7 <clarencejoseph.dimafelix@gmail.com>
2022-10-14 18:21:57 +0800

    Revert "added backend code"
    This reverts commit 52f9e3148142e5edd556086e55ea5a14aaf3b388.

--- public_html/login.php
+++ public_html/login.php
@@ -6,12 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Snackwise</title>
+
     <!-- BOOTSTRAP CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <link rel="stylesheet" href="./css/login.css">
+
 </head>
+
 <body>
+
     <div class="parent-container">
+
         <nav class="navbar navbar-light navbar-expand-md bg-light">
             <div class="container">
                 <a href="index.php" class="navbar-brand">
@@ -46,6 +52,7 @@
                 </div>
             </div>
         </nav>
+
         <div class="container bg-light form-container">
             <form action="">
                 <div class="row">
@@ -77,10 +84,18 @@
                         </div>
                     </div>
                 </div>
+
             </form>
         </div>
+
     </div>
+
+
     <!-- BOOTSTRAP JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+
 </body>
+
 </html>
