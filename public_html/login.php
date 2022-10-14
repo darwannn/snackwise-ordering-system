ed9bbe70d0ff33d3782118c04b7e98d7423f0f9e
clarencetinator7 <clarencejoseph.dimafelix@gmail.com>
2022-10-14 18:20:26 +0800

    Update login.php

--- public_html/login.php
+++ public_html/login.php
@@ -8,7 +8,13 @@
     <!-- BOOTSTRAP CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <<<<<<< HEAD
+<<<<<<< HEAD
     <link rel="stylesheet" href="./css/login.css">
+=======
+
+    
+    
+>>>>>>> parent of 8578f0d (Initial Sign In Forms Added)
 =======
 
     
@@ -52,6 +58,7 @@
             </div>
         </nav>
 <<<<<<< HEAD
+<<<<<<< HEAD
         <div class="container bg-light form-container">
             <form action="">
                 <div class="row">
@@ -89,6 +96,8 @@
     <!-- BOOTSTRAP JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 =======
+=======
+>>>>>>> parent of 8578f0d (Initial Sign In Forms Added)
        
     </div>
 
@@ -98,6 +107,9 @@
 
     
 
+<<<<<<< HEAD
+>>>>>>> parent of 8578f0d (Initial Sign In Forms Added)
+=======
 >>>>>>> parent of 8578f0d (Initial Sign In Forms Added)
 </body>
 </html>
