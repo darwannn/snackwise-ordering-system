e2f2747b6c82cd6a3518e130e51ce50fb49668e7
clarencetinator7 <clarencejoseph.dimafelix@gmail.com>
2022-10-14 18:22:09 +0800

    Revert "Initial Sign In Forms Added"
    This reverts commit 8578f0d4f061c45f09028e26356ae6a84be6ca64.

--- public_html/login.php
+++ public_html/login.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,16 +9,15 @@
     <!-- BOOTSTRAP CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="./css/login.css">
-
+    
+    
 </head>
-
 <body>
 
     <div class="parent-container">
 
         <nav class="navbar navbar-light navbar-expand-md bg-light">
-            <div class="container">
+            <div class="container-fluid">
                 <a href="index.php" class="navbar-brand">
                     <img src="./img/penguin.png" alt="Penguin Logo" height="58" width="52">
                     SNACKWISE
@@ -52,50 +50,14 @@
                 </div>
             </div>
         </nav>
-
-        <div class="container bg-light form-container">
-            <form action="">
-                <div class="row">
-                    <h3>Sign In</h3>
-                    <span>Enter your account details to continue</span>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <input type="email" class="form-control m-2" name="email" id="" placeholder="Enter Email Address">
-                    </div>
-                    <div class="col-12">
-                        <input type="text" class="form-control m-2" name="firstName" id="" placeholder="Enter Password">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <a href="#" class="">Forgot Password?</a>
-                    </div> 
-                </div>
-                <div class="row">
-                    <div class="d-grid gap-2">
-                      <button type="button" name="" id="" class="btn btn-primary">Sign In</button>
-                    </div> 
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <div class="d-grid gap-2">
-                          <button type="button" name="" id="" class="btn btn-warning">Create an Account</button>
-                        </div>
-                    </div>
-                </div>
-
-            </form>
-        </div>
-
+       
     </div>
 
-
+     
     <!-- BOOTSTRAP JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-
+    
 
 </body>
-
 </html>
