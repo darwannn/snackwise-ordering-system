9a6142017b20802747e917aa4843880482b26d65
clarencetinator7 <clarencejoseph.dimafelix@gmail.com>
2022-11-14 22:08:38 +0800

    Added new-password front end

--- public_html/account/new-password.php
+++ public_html/account/new-password.php
@@ -1,81 +1,136 @@
 <?php
-require_once dirname(__FILE__). "/../php/classes/Account.php";
-require_once dirname(__FILE__). "/../php/classes/Validate.php";
+require_once dirname(__FILE__) . "/../php/classes/Account.php";
+require_once dirname(__FILE__) . "/../php/classes/Validate.php";
 
 $account = new Account();
 
-
 $validate = new Validate();
 /* if ($validate->validate_code() || $validate->validate_code() != null) { */
-    if (!$validate->validate_code() ) {
-header('error.php');
-}
+// if (!$validate->validate_code()) {
+//     header('error.php');
+// }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <title>SL Visuals</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>New Password | Snackwise</title>
+    <!-- PAGE ICON -->
+    <link rel="icon" href="../img/penguin.png" type="image/icon type">
+
+    <!-- FONT LINKS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&family=Poppins:ital,wght@0,300;0,600;0,700;1,400&family=Roboto:ital,wght@0,300;0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+    <!-- BOOTSTRAP CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- BOOTSTRAP JS  -->
+
+
+    <!-- FONT AWESOME -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js"></script>
+
+    <link rel="stylesheet" href="../css/new-password.css">
+
 </head>
 
 <body>
 
-<form id="account_form" method="POST">
+    <!-- <form id="account_form" method="POST">
 
-<span id="success_message"></span>
-<span id="error_message"></span>
+        <span id="success_message"></span>
+        <span id="error_message"></span>
 
 
-<div class="">
-    <label for="password">Password</label>
-    <input type="password" class="password" name="password" id="password" placeholder="Password" value="" 
-    onkeyup="new Account().verify_password(this.value);" autocomplete="off">
-    <i class="fa-solid fa-eye-slash" id="password_toggler" for="password" onclick="new Account().toggle_password(this.id, this.getAttribute('for'))"></i>
-    <span class="" id="password_error"></span>
-    
-    <div class="password_requirements">
-                    <h6><span class="length " id="length" style="opacity: 0;">&#x2716;</span>8 to 16 characters</h6>
-                    <h6><span class="case " id="case" style="opacity: 0;">&#x2716;</span>At least 1 Uppercase</h6>
-                    <h6><span class="number " id="number" style="opacity: 0;">&#x2716;</span>At least 1 number</h6>
-                    <h6><span class="special " id="special" style="opacity: 0;">&#x2716;</span>At least 1 special</h6>
+        <div class="">
+            <label for="password">Password</label>
+            <input type="password" class="password" name="password" id="password" placeholder="Password" value="" onkeyup="new Account().verify_password(this.value);" autocomplete="off">
+            <i class="fa-solid fa-eye-slash" id="password_toggler" for="password" onclick="new Account().toggle_password(this.id, this.getAttribute('for'))"></i>
+            <span class="" id="password_error"></span>
+
+            <div class="password_requirements">
+                <h6><span class="length " id="length" style="opacity: 0;">&#x2716;</span>8 to 16 characters</h6>
+                <h6><span class="case " id="case" style="opacity: 0;">&#x2716;</span>At least 1 Uppercase</h6>
+                <h6><span class="number " id="number" style="opacity: 0;">&#x2716;</span>At least 1 number</h6>
+                <h6><span class="special " id="special" style="opacity: 0;">&#x2716;</span>At least 1 special</h6>
+            </div>
+
+        </div>
+
+        <div class="">
+            <label for="retype_password">Retype Password</label>
+            <input type="password" class="retype_password" name="retype_password" id="retype_password" placeholder="Retype Password" value="" autocomplete="off">
+            <i class="fa-solid fa-eye-slash" id="retype_password_toggler" for="retype_password" onclick="new Account().toggle_password(this.id, this.getAttribute('for'))"></i>
+            <span class="" id="retype_password_error"></span>
+        </div>
+
+
+        </div>
+
+        <button type="button" id="new_password" class=""> New Password</button>
+
+
+    </form> -->
+
+    <div class="parent-container">
+
+        <div class="window-container">
+            <div class="snackwise-label">
+                <div class="brand-icon">
+                    <img src="../img/penguin.png" class="snackwise-icon" alt="Snackwise Logo">
                 </div>
+                <div class="brand-name">
+                    <a href="../index.php">
+                        <span class="red">SNACK<span class="yellow">WISE</span></span>
+                    </a>
+                </div>
+            </div>
+            <div class="form-header">
+                <h1>Create New Password</h1>
+            </div>
+            <form action="POST" id="account_form">
+                <div class="input-container">
+                    <input type="password" class="password" name="password" id="password" placeholder="Password" value="" onkeyup="new Account().verify_password(this.value);" autocomplete="off">
+                    <i class="fa-solid fa-eye-slash toggler" id="password_toggler" for="password" onclick="new Account().toggle_password(this.id, this.getAttribute('for'))"></i>
+                    <span class="" id="password_error" class="text-danger"></span>
+                </div>
+                <div class="input-container">
+                    <input type="password" class="retype_password" name="retype_password" id="retype_password" placeholder="Retype Password" value="" autocomplete="off">
+                    <i class="fa-solid fa-eye-slash toggler" id="retype_password_toggler" for="retype_password" onclick="new Account().toggle_password(this.id, this.getAttribute('for'))"></i>
+                    <span class="" id="retype_password_error" class="text-danger"></span>
+                </div>
+                <button id="new_password" class="new-password-btn">Submit New Password</button>
+            </form>
+        </div>
 
-</div>
+    </div>
 
-<div class="">
-    <label for="retype_password">Retype Password</label>
-    <input type="password" class="retype_password" name="retype_password" id="retype_password"
-        placeholder="Retype Password" value="" autocomplete="off">
-        <i class="fa-solid fa-eye-slash" id="retype_password_toggler" for="retype_password" onclick="new Account().toggle_password(this.id, this.getAttribute('for'))"></i>
-    <span class="" id="retype_password_error"></span>
-</div>
+    <!-- BOOTSTRAP JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
+    <script src="../js/Account.js"></script>
 
-</div>
+    <script>
+        //TEMPORARY
+        // HIDE PASSWORD REQUIREMENTS
 
-<button type="button" id="new_password" class=""> New Password</button>
+        document.getElementsByClassName("password_requirements").style.display = "none";
 
+        let account = new Account();
 
-</form>
-<script src="../js/Account.js" ></script>
-<script>
-    let account = new Account();
-
-    let url_code = '<?php echo $_GET['code']?>';
-    document.getElementById("new_password").onclick = function () {
-         console.log(url_code);
+        let url_code = '<?php echo $_GET['code'] ?>';
+        document.getElementById("new_password").onclick = function() {
+            console.log(url_code);
             account.new_password(url_code);
         }
-
-
-  
-
-
-
-</script>
+    </script>
 </body>
 
 </html>
