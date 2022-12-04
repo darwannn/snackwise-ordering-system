e8b16fcfbe7e0776f79b2914a065df607d662506
clarencetinator7 <clarencejoseph.dimafelix@gmail.com>
2022-12-04 23:42:21 +0800

    add: change-password front end

--- public_html/change-password.php
+++ public_html/change-password.php
@@ -117,8 +117,56 @@ if ($validate->is_logged_in("customer")) {
     </form> -->
 
 
+    <div class="parent-container">
+        <div class="form-window">
+            <a href="../index.php" class="snackwise-label text-decoration-none">
+                <div class="brand-icon">
+                    <img src="../img/penguin.png" class="snackwise-icon" alt="Snackwise Logo">
+                </div>
+                <div class="brand-name">
+                    <span class="red">SNACK<span class="yellow">WISE</span></span>
+                </div>
+            </a>
+            <div class="back-container">
+                <a href="index.php"><i class="fa-solid fa-arrow-left"></i> Back to Home</a>
+            </div>
+            <div class="form-header">
+                <h1>Forgot Password</h1>
+            </div>
+            <form action="POST" id="account_form">
 
-    
+                <div class="input-container">
+                    <input type="password" class="current_password" name="current_password" id="current_password" placeholder="Retype Password" value="" autocomplete="off">
+                    <i class="fa-solid fa-eye-slash toggler" id="current_password_toggler" for="current_password" onclick="new Account().toggle_password(this.id, this.getAttribute('for'))"></i>
+                    <span class="input_error" id="current_password_error" class="text-danger"></span>
+                </div>
+
+                <span class="group-txt">Set New Password:</span>
+                <div class="input-container">
+                    <input type="password" class="password" name="password" id="password" placeholder="New Password" value="" onkeyup="new Account().verify_password(this.value);" autocomplete="off">
+                    <i class="fa-solid fa-eye-slash toggler" id="password_toggler" for="password" onclick="new Account().toggle_password(this.id, this.getAttribute('for'))"></i>
+                    <span class="input_error" id="password_error" class="text-danger"></span>
+
+                    <div class="password_requirements">
+                        <h6 id="length_con"><span class="length me-1" id="length">&#x2716;</span> be at least 8 characters but not more than 20</h6>
+                        <h6 id="case_con"><span class="case me-1" id="case">&#x2716;</span> contain at least one number, </h6>
+                        <h6 id="number_con"><span class="number me-1" id="number">&#x2716;</span> contain at least one uppercase and lowercase letter</h6>
+                        <h6 id="special_con"><span class="special me-1" id="special">&#x2716;</span> contain one of the following characters: @ . # $ % ^ & , *</h6>
+                    </div>
+                </div>
+
+                <div class="input-container">
+                    <input type="password" class="retype_password" name="retype_password" id="retype_password" placeholder="Retype New Password" value="" autocomplete="off">
+                    <i class="fa-solid fa-eye-slash toggler" id="retype_password_toggler" for="retype_password" onclick="new Account().toggle_password(this.id, this.getAttribute('for'))"></i>
+                    <span class="input_error" id="retype_password_error" class="text-danger"></span>
+                </div>
+                <button type="button" id="change_password" class="btn btn-outline-danger update-btn" onclick="new Account().change_password();"> Change Password</button>
+
+
+            </form>
+        </div>
+    </div>
+
 
     <!-- toast_notif notification will be appended here -->
     <div class="toast_notif" id="toast_notif"></div>
