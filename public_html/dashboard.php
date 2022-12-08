2ab30a5b0271771354695d44d5a8b58417c53798
clarencetinator7 <clarencejoseph.dimafelix@gmail.com>
2022-12-08 14:22:40 +0800

    add: dashboard card template

--- public_html/dashboard.php
+++ public_html/dashboard.php
@@ -15,12 +15,13 @@ $account->delete_code();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard | Snackwise</title>
-    
+
     <!-- PAGE ICON -->
     <link rel="icon" href="img/penguin.png" type="image/icon type">
 
@@ -41,8 +42,9 @@ $account->delete_code();
     <link rel="stylesheet" href="css/dashboard.css">
     <link rel="stylesheet" href="css/notification.css">
 </head>
+
 <body>
-  
+
     <div class="back-pattern"></div>
 
     <div class="wrapper">
@@ -57,44 +59,43 @@ $account->delete_code();
                     <img src="https://res.cloudinary.com/dhzn9musm/image/upload/<?php echo $_SESSION['current_image'] ?>" alt="">
                 </div>
                 <div class="user-info">
-                    <a href="profile.php"><span class="welcome-txt">Welcome,<br><span class="first-name"><?php echo $_SESSION['current_firstname']?></span></span></a>
+                    <a href="profile.php"><span class="welcome-txt">Welcome,<br><span class="first-name"><?php echo $_SESSION['current_firstname'] ?></span></span></a>
                 </div>
             </div>
             <div class="navigation-container">
-                    <ul class="navigation">
-                        <li class="nav-item" id="active-nav">
-                            <a href="dashboard.php"><i class="fa-solid fa-chart-pie"></i> Dashboard</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="edit-order.php"><i class="fa-solid fa-receipt"></i> Edit Orders</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="edit-menu.php"><i class="fa-solid fa-burger"></i> Edit Menu</a>
-                        </li>
-                        <!-- DI PA SURE TO --> 
-                        <!-- <li class="nav-item">
+                <ul class="navigation">
+                    <li class="nav-item" id="active-nav">
+                        <a href="dashboard.php"><i class="fa-solid fa-chart-pie"></i> Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="edit-order.php"><i class="fa-solid fa-receipt"></i> Edit Orders</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="edit-menu.php"><i class="fa-solid fa-burger"></i> Edit Menu</a>
+                    </li>
+                    <!-- DI PA SURE TO -->
+                    <!-- <li class="nav-item">
                             <a href="edit-users.php"><i class="fa-solid fa-user"></i> Edit Users</a>
                         </li> -->
-                    </ul>
-                </div>
-                <div class="bottom-btn">
-                    <a href="account/logout.php"><i class="fa-solid fa-right-from-bracket"></i> Logout</a>
-                </div>
+                </ul>
+            </div>
+            <div class="bottom-btn">
+                <a href="account/logout.php"><i class="fa-solid fa-right-from-bracket"></i> Logout</a>
+            </div>
         </div>
         <div class="content">
             <div class="content-header">
-               <h1>Dashboard</h1> 
+                <h1>Dashboard</h1>
             </div>
-            <div class="cards-container">
-                <div class="cards">
+            <div class="cards-row">
+                <div class="dashb-card">
                     <div class="card-info">
                         <span class="card-title">This week's total completed order:</span>
                         <span class="total-number" id="weekly-total">00</span>
                         <span class="additional-note">Cancelled order not included</span>
                     </div>
-                    
+                    <div class="bottom-line"></div>
                 </div>
-
             </div>
         </div>
     </div>
@@ -104,4 +105,5 @@ $account->delete_code();
 
 
 </body>
+
 </html>
