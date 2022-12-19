ff178ffc6de2176d2dd337a0020b28348b9f399c
darwin <darwinsanluisramos21@gmail.com>
2022-12-19 18:48:48 +0800

    update report

--- public_html/report.php
+++ public_html/report.php
@@ -19,6 +19,7 @@ if($filter == "alltime") {
 	FROM transaction 
 	INNER JOIN user ON user.user_id = transaction.user_id
 	ORDER BY transaction_id DESC';
+	$date_span = "";
 }
 if($filter == "thisweek") {
 	$query = 'SELECT transaction_id, CONCAT(user.firstname, " ", user.lastname) AS fullname, order_id, date, total_price 
@@ -26,6 +27,7 @@ if($filter == "thisweek") {
 	INNER JOIN user ON user.user_id = transaction.user_id 
 	WHERE WEEK(date) = WEEK("'.$currentDate.'") AND YEAR(date) = YEAR("'.$currentDate.'")
 	ORDER BY transaction_id DESC';
+	$date_span = "Weekly";
 }
 if($filter == "thismonth") {
 	$query = 'SELECT transaction_id, CONCAT(user.firstname, " ", user.lastname) AS fullname, order_id, date, total_price 
@@ -33,6 +35,7 @@ if($filter == "thismonth") {
 	INNER JOIN user ON user.user_id = transaction.user_id 
 	WHERE MONTH(date) = MONTH("'.$currentDate.'") AND YEAR(date) = YEAR("'.$currentDate.'")
 	ORDER BY transaction_id DESC';
+	$date_span = "Monthly";
 }
 if($filter == "thisyear") {
 	$query = 'SELECT transaction_id, CONCAT(user.firstname, " ", user.lastname) AS fullname, order_id, date, total_price 
@@ -40,6 +43,7 @@ if($filter == "thisyear") {
 	INNER JOIN user ON user.user_id = transaction.user_id 
 	WHERE YEAR(date) = YEAR("'.$currentDate.'")
 	ORDER BY transaction_id DESC';
+	$date_span = "Annual";
 }
 
 
@@ -52,12 +56,17 @@ $result  =  $stmt->execute();
 
 if ($stmt->rowCount() > 0) {
 	$delay = 100;
-	$html.='<div>Sales Report</div>';
-	$html.='<div>Zone 3, Stall 1 Sto. Rosario Hagonoy Bulacan</div>';
-	$html.='<div>Date Generated: '. date('F d, Y', strtotime($currentDate)).'</div>';
+
+	$html.='<body style="width:100%;">';
+	$html.='<div style="text-align:center;">  <h1><span style="color: #DD1C1A;">SNACK</span><span
+	style="color: #F0C808; text-align: center;">WISE</span></h1></div>';
+	$html.='<div style="text-align:center;">'.$date_span.' Sales Report</div>';
+	$html.='<div style="text-align:center;">Zone 3, Stall 1 Sto. Rosario Hagonoy Bulacan</div>';
+	$html.='<br><br>';
+	$html.='<div style="text-align:left;">Date Generated: '. date('F d, Y', strtotime($currentDate)).'</div>';
 	$html.='<br>';
 
-	$html.='<table class="table">';
+	$html.='<table class="table" style="width:100%;">';
 	$html.='<tr><td>#</td> <td>Transaction ID</td> <td>Customer Name</td> <td>Order ID</td> <td>Date of Order</td> <td>Total Paid</td> </tr>';
 	while ($row = $stmt->fetch(PDO::FETCH_BOTH)) {
 		$counter++;
@@ -66,9 +75,10 @@ if ($stmt->rowCount() > 0) {
 	}
 
 	$html.='</table>';
-	$html.='<br>';
-	$html.='<div>Total Sales: PHP '. number_format($sales, 2, '.', ',').'</div>';
-	$html.='<div>Total Transaction: '. $counter.'</div>';
+	$html.='<br><br>';
+	$html.='<div style="text-align:right;">Total Sales: PHP '. number_format($sales, 2, '.', ',').'</div>';
+	$html.='<div style="text-align:right;">Total Transaction: '. $counter.'</div>';
+	$html.='</body>';
 }else{
 	$html="No records found";
 }
